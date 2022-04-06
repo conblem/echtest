18bb95cb28dd30958f6ce6b7958223e2afde8abc
Ramon Wehrli <mail@conblem.me>
2022-04-06 13:05:08 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"circl/kem"
+	"fmt"
+)
+
+type iECHConfigBuilder interface {
+	setVersion(version uint16)
+	setConfigId(configId uint8)
+}
+
+type ECHConfigBuilder struct {
+	version uint16
+	configId uint8
+	publicKey *kem.PublicKey
+}
+
+func (b *ECHConfigBuilder) setVersion(version uint16) {
+	b.version = version
+}
+
+func (b *ECHConfigBuilder) setConfigId(configId uint8) {
+	b.configId = configId
+}
+
+type ECHConfigInner struct {
+
+}
+
+
+func main() {
+	fmt.Println("Hello, World! 222")
+}
